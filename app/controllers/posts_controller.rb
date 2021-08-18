9e409aada68d936d777956321e22b6f083f98e15
h tsuruoka <hi.violet.18@gmail.com>
2021-08-19 00:24:53 +0900

    投稿削除機能作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,4 +21,9 @@ class PostsController < ApplicationController
     @post.save
     redirect_to("/posts/index")
   end
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post "posts/create", to: "posts#create"
   get "posts/:id/edit", to: "posts#edit"
   post "posts/:id/update", to: "posts#update"
+  post "posts/:id/destroy", to: "posts#destroy"
   get "/", to: "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
