f4749e353dc871a71546bda7ea41468ed61aebc1
h tsuruoka <hi.violet.18@gmail.com>
2021-08-19 03:08:13 +0900

    最終確認

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,4 @@ Rails.application.routes.draw do
   post "posts/:id/update", to: "posts#update"
   post "posts/:id/destroy", to: "posts#destroy"
   get "/", to: "home#top"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
