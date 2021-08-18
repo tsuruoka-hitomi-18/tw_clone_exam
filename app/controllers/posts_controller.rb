857bfa1aea98b7b663259acdba71d54a59326dec
h tsuruoka <hi.violet.18@gmail.com>
2021-08-19 00:01:09 +0900

    投稿編集機能の完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,13 @@ class PostsController < ApplicationController
     @post.save
     redirect_to("/posts/index")
   end
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get "posts/new", to: "posts#new"
   get "posts/:id", to: "posts#show"
   post "posts/create", to: "posts#create"
+  get "posts/:id/edit", to: "posts#edit"
+  post "posts/:id/update", to: "posts#update"
   get "/", to: "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
