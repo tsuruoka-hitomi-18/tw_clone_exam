dc3ba0a9cf52a54a67fa68a825879f6f5793bb9b
h tsuruoka <hi.violet.18@gmail.com>
2021-08-18 22:27:08 +0900

    新規投稿ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "posts/index", to: "posts#index"
+  get "posts/new", to: "posts#new"
+  get "posts/:id", to: "posts#show"
+  post "posts/create", to: "posts#create"
   get "/", to: "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
