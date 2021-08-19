bc73625be53c7c2b985f34329f4c6b2a6645cf31
h tsuruoka <hi.violet.18@gmail.com>
2021-08-19 21:19:42 +0900

    サクセスメッセージ作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,16 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
   end
   def new
+    @post = Post.new
   end
   def create
     @post = Post.new(content: params[:content])
-    @post.save
-    redirect_to("/posts/index")
+    if @post.save 
+      flash[:notice] = "投稿を作成しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
   end
   def edit
     @post = Post.find_by(id: params[:id])
@@ -19,6 +24,7 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
     @post.content = params[:content]
     if @post.save
+      flash[:notice] = "投稿を編集しました"
       redirect_to("/posts/index")
     else
       render("posts/edit")
@@ -27,6 +33,7 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find_by(id: params[:id])
     @post.destroy
+    flash[:notice] = "投稿を削除しました"
     redirect_to("/posts/index")
   end
 end
